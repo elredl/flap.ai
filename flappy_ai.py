228f7a9f97132e89f7574a2f747461d612878a28
elredl <leonardo.rodolico03@gmail.com>
2025-10-30 14:37:38 -0600

    Initialized game + alg

---
+++ flappy_ai.py
@@ -0,0 +1,10 @@
+import gymnasium as gym
+
+# environment: flappy bird
+# state: player (y, y_vel), pipe(x, bottom-top-y, top-bottom-y), score
+# actions: flap, no_flap
+# reward: time alive/score (synonimous)
+# return: if next pipe is higher/lower than current (?)
+# episode: 100
+
+
